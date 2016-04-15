2a92d8a1f3e251147d834d8372d96870a283d3de
Vivek Parekh <vivekparekh8@gmail.com>
2016-04-15 16:28:38 -0500

    Put Post methods for User and Product

--- readme.md
+++
@@ -1,4 +0,0 @@
-Steps:
-1. git clone
-2. npm install
-3. npm start
--- routes/helloworld.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-/* GET helloworld */
-router.get('/', function(req, res, next) {
-  res.render('helloworld', { message: 'Hello World', name: 'Shobhit' })
-})
-
-module.exports = router
